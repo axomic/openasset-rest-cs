5c69c22a7ac48e42760bdfd99ceb771d1f97fd75
duarte-aragao-axomic <duarte.aragao@axomic.com>
2013-02-20 17:59:03 +0000

    Still haven't tested the Delete method. Need to do functions that don't need validation.

---
+++ OpenAsset.RestClient.Library/PostResponse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+// could have used the JavaScriptSerializer but the Newtonsoft seems to be faster
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json.Linq;
+using System.Runtime.Serialization;
+
+namespace OARestClientLib
+{
+    [JsonObject(MemberSerialization.OptIn)]
+    public class PostResponse
+    {
+        [JsonProperty("new_id", NullValueHandling = NullValueHandling.Ignore)]
+        protected int NewId { get; set; }
+        [JsonProperty("message", NullValueHandling = NullValueHandling.Ignore)]
+        protected string Message { get; set; }
+        [JsonProperty("code", NullValueHandling = NullValueHandling.Ignore)]
+        protected int Code { get; set; }
+    }
+}
