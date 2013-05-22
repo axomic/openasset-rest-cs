5852723078407a138abf67a0072d656fba4b0cb1
Justin Kidman <justin.kidman@axomic.com>
2013-05-22 10:00:27 -0400

    Cleanup of naming and AssemblyInfo

--- OpenAsset.RestClient.Library/Properties/AssemblyInfo.cs
+++ OpenAsset.RestClient.Library/Properties/AssemblyInfo.cs
@@ -8,11 +8,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("OpenAsset.RestClient.Library")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Axomic Ltd")]
-[assembly: AssemblyProduct("OpenAsset RestClient Library")]
-[assembly: AssemblyCopyright("Copyright © Axomic Ltd 2013")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -21,16 +16,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("137fd3b7-9fa8-4003-a734-bb82cf0e2586")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
