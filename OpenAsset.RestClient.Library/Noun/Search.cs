565710fb70f1c114b6b35ed93743ec0c68b2843d
Duarte Aragão <daragao@gmail.com>
2013-12-16 14:19:24 +0000

    add search

---
+++ OpenAsset.RestClient.Library/Noun/Search.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+// serialization stuff
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json.Linq;
+using System.Runtime.Serialization;
+
+namespace OpenAsset.RestClient.Library.Noun
+{
+    public class Search : Base.BaseNoun
+    {
+        [JsonProperty]
+        public string name;
+        [JsonProperty]
+        public int all_users_can_modify;
+        [JsonProperty]
+        public int approved_company_saved_search;
+        [JsonProperty]
+        public int can_modify;
+        [JsonProperty]
+        public string code;
+        [JsonProperty]
+        public int company_saved_search;
+        [JsonProperty]
+        public string created;
+        [JsonProperty]
+        public int locked;
+        [JsonProperty]
+        public int saved;
+        [JsonProperty]
+        public int share_with_all_users;
+        [JsonProperty]
+        public string updated;
+        [JsonProperty]
+        public int user_id;
+        [JsonProperty]
+        public List<SearchItem> search_items;
+
+        public override string UniqueCode
+        {
+            get { return code; }
+            set { code = value; }
+        }
+
+        public override string UniqueCodeField
+        {
+            get { return "code"; }
+        }
+
+        public bool MatchingSearchItems(List<SearchItem> searchItems)
+        {
+            if (search_items == null)
+                return searchItems == null;
+            if (searchItems == null)
+                return false;
+            if (search_items.Count != searchItems.Count)
+                return false;
+            for (int i = 0; i < searchItems.Count; i++)
+            {
+                if (!searchItems[i].Equals(search_items[i]))
+                    return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ OpenAsset.RestClient.Library/Noun/SearchItem.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+// serialization stuff
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json.Linq;
+using System.Runtime.Serialization;
+
+namespace OpenAsset.RestClient.Library.Noun
+{
+    public class SearchItem : Base.BaseNoun
+    {
+        [JsonProperty]
+        public string code;
+        [JsonProperty("protected")]
+        public string _operator;
+        [JsonProperty]
+        public int exclude;
+        [JsonProperty]
+        public List<int> ids;
+        [JsonProperty]
+        public List<string> values;
+
+        public SearchItem()
+        {
+            exclude = 0;
+        }
+
+        public override bool Equals(object obj)
+        {
+            SearchItem otherObj = obj as SearchItem;
+
+            if (otherObj == null)
+                return false;
+            // Code
+            if (!this.code.Equals(otherObj.code))
+                return false;
+            // Exclude
+            if (this.exclude != otherObj.exclude)
+                return false;
+            // _operator
+            if (String.IsNullOrEmpty(this._operator) || this._operator.Equals("OR"))
+            {
+                // This is because empty/null is considered OR by REST APIs
+                if (!String.IsNullOrEmpty(otherObj._operator) && !otherObj._operator.Equals("OR"))
+                    return false;
+            }
+            else
+            {
+                if (!this._operator.Equals(otherObj._operator))
+                    return false;
+            }
+            // ids
+            if (this.ids != null)
+            {
+                if (otherObj.ids == null)
+                    return false;
+                if (!this.ids.SequenceEqual(otherObj.ids))
+                    return false;
+            }
+            else if (otherObj.ids != null)
+            {
+                return false;
+            }
+            // values
+            if (this.values != null)
+            {
+                if (otherObj.values == null)
+                    return false;
+                if (!this.values.SequenceEqual(otherObj.values))
+                    return false;
+            }
+            else if (otherObj.values != null)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = 0;
+
+            hashCode ^= code.GetHashCode();
+            hashCode ^= exclude.GetHashCode();
+            if (String.IsNullOrEmpty(this._operator))
+                hashCode ^= "OR".GetHashCode();
+            else
+                hashCode ^= _operator.GetHashCode();
+            if (ids != null)
+            {
+                foreach (int id in ids)
+                    hashCode ^= id.GetHashCode();
+            }
+            if (values != null)
+            {
+                foreach (string value in values)
+                    hashCode ^= value.GetHashCode();
+            }
+            return hashCode;
+        }
+        /*
+        public override string ToString()
+        {
+            string text = Char.ToUpperInvariant(code[0]) + code.Substring(1);
+            int id = 0;
+            if (code.Contains('.'))
+            {
+                id = Convert.ToInt32(text.Substring(text.IndexOf('.') + 1));
+                text = text.Substring(0, text.IndexOf('.'));
+                switch (text)
+                {
+                    case "Field":
+                        Field field = RESTEngine.Instance.GetField(id);
+                        if (field != null)
+                            text = field.name;
+                        break;
+                    case "Keyword":
+                        KeywordCategory keywordCategory = RESTEngine.Instance.GetKeywordCategory(id);
+                        if (keywordCategory != null)
+                            text = keywordCategory.name;
+                        break;
+                    case "ProjectKeyword":
+                        ProjectKeywordCategory projectKeywordCategory = RESTEngine.Instance.GetProjectKeywordCategory(id);
+                        if (projectKeywordCategory != null)
+                            text = projectKeywordCategory.name;
+                        break;
+                }
+            }
+            text += ": ";
+            if (values != null && values.Count > 0)
+            {
+                text += String.Join(", ", values.ToArray());
+            }
+            if (ids != null && ids.Count > 0)
+            {
+                List<string> idStrings = BLL.RESTEngine.Instance.GetSearchStringsFromIds(code, ids);
+                text += String.Join(", ", idStrings.ToArray());
+            }
+            return text;
+        }
+        */
+        public override int CompareTo(object obj)
+        {
+            if (obj == null) return 1;
+
+            SearchItem otherSearchItem = obj as SearchItem;
+            if (otherSearchItem != null)
+                return this.code.CompareTo(otherSearchItem.code);
+            else
+                throw new ArgumentException("Object is not a SearchItem");
+        }
+    }
+}
