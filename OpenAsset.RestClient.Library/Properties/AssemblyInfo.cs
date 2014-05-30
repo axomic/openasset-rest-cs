a632cc315b768ab7ca4d5dab532d62de7a078988
Justin Kidman <justin.kidman@axomic.com>
2014-05-30 17:21:03 -0400

    Ensure Assembly Version will not have a typo in use for installers.

--- OpenAsset.RestClient.Library/Properties/AssemblyInfo.cs
+++ OpenAsset.RestClient.Library/Properties/AssemblyInfo.cs
@@ -32,5 +32,10 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.4.0")]
-[assembly: AssemblyFileVersion("1.0.4.0")]
+[assembly: AssemblyVersion(AssemblyVersion.Version)]
+[assembly: AssemblyFileVersion(AssemblyVersion.Version)]
+
+internal static class AssemblyVersion
+{
+    public const string Version = "1.0.4.0";
+}
